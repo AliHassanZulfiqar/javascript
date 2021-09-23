56d887dba0c3dec08ebfe53f506bfb35f494ce43
Ali Hassan <bc200401060@vu.edu.pk>
2021-09-23 07:22:02 +0500

    var, let, constants/Section-1

--- js_fundamentals/app.js
+++ js_fundamentals/app.js
@@ -1,7 +1,7 @@
-// alert('Hello World')
+// 1: alert('Hello World')
 
-// Log to console 
-
+// 2: Log to console 
+/*
 console.log('Hello World');
 console.log('123');
 console.log(true);
@@ -21,3 +21,53 @@ console.time('HELLO'); //time
   console.log('Hello World');
   console.log('Hello World');
 console.timeEnd('HELLO End'); //time
+*/
+
+// 3: var, let, const
+
+// var:
+
+var name = 'Ali Hassan'; //declaring
+var id = 12;
+console.log(name);
+name = 'Johny'; //reassigning
+console.log(name);
+
+var age; //Initializing
+console.log(age);
+age = 23;
+console.log(age);
+
+// Allowed "letters, numbers, _, $"
+// Cannot start with a number
+
+var firstName = 'John'; // Camel case
+var first_name = 'John'; // Underscore
+var FirstName = 'John'; // Pascal Case
+
+// let:
+
+let name;
+let name = 'Ali Hassan'; //declaring
+let id = 12;
+console.log(name);
+name = 'Johny'; //reassigning
+console.log(name);
+
+// const:
+
+const name = 'Ali'; //cannot be reassigned, have to be assigned with a value
+console.log(name);
+
+const person = {
+  name = 'Ali',
+  age = 30
+};
+
+person.name = 'Hassan'; // const cannot be reassigned but it's objects can be.
+person.age = '20';
+console.log(person);
+
+const num = [1, 2, 3, 4, 5];
+num.push(6); // same goes for arrays, here we adding a number to array
+console.log(num);
