1d7182d84cd31624f5dfcaa3bc321caeea05c417
Ali Hassan <bc200401060@vu.edu.pk>
2021-09-23 06:43:52 +0500

    console variable practice/Section-1

--- js_fundamentals/app.js
+++ js_fundamentals/app.js
@@ -1,1 +1,23 @@
-alert('Hello World!')
+// alert('Hello World')
+
+// Log to console 
+
+console.log('Hello World');
+console.log('123');
+console.log(true);
+var greeting = 'Hello'
+console.log(greeting);
+console.log([1,2,3,4]); //Array
+console.log({a:1, b:2}); //object
+console.table({a:1, b:2}); //table
+console.error('Error 404'); //Errors
+// console.clear(); //Clear Console
+console.warn('Warning!'); //warning
+console.time('HELLO'); //time
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+console.timeEnd('HELLO End'); //time
