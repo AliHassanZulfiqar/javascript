e2d9eca2644462ae70b0037e071ccb57eed308aa
Ali Hassan <bc200401060@vu.edu.pk>
2021-09-27 23:59:04 +0500

    Document Object Model /Section-2

---
+++ js_DOM_Manipulation_events/app.js
@@ -0,0 +1,49 @@
+// 1: Document Object properties
+
+let val;
+
+val = document;
+val = document.all;
+val = document.all(5);
+val = document.all.length;
+
+console.log(val);let val;
+
+val = document;
+val = document.all;
+val = document.all[2];
+val = document.all.length;
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.contentType;
+
+val = document.forms;
+val = document.forms[0];
+val = document.forms[0].id;
+val = document.forms[0].method;
+val = document.forms[0].action;
+
+val = document.links;
+val = document.links[0];
+val = document.links[0].id;
+val = document.links[0].className; // it shows us the classes
+val = document.links[0].classList[0];
+
+val = document.images;
+
+val = document.scripts;
+val = document.scripts[2].getAttribute('src');
+
+let scripts = document.scripts;
+
+let scriptsArr = Array.from(scripts);
+
+scriptsArr.forEach(function(script) {
+  console.log(script.getAttribute('src'));
+});
+
+console.log(val);
