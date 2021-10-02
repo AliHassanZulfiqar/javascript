20aa264ed23bf6c37d3a7c6bdaa7693e02e0569e
Ali Hassan <bc200401060@vu.edu.pk>
2021-10-02 20:27:01 +0500

    DOM element create /Section-2

--- js_DOM_Manipulation_events/app.js
+++ js_DOM_Manipulation_events/app.js
@@ -89,6 +89,7 @@ document.querySelector('li:nth-child(odd)').style.background = '#333'; // it wil
 
 // 2: Multiple Element Selectors
 
+/*
 // document.getElementsByClassName
 // const item = document.getElementsByClassName('collection-item'); //this selects all the class items in the whole document but we should not do it on a global scope
 // console.log(item);
@@ -139,3 +140,61 @@ for(let i=0; i < liEven.length; i++){
 };
 
 console.log(items);
+*/
+
+// 3: DOM Element Creation
+
+let val;
+
+const list = document.querySelector('ul.collection');
+const listItem = document.querySelector('li.collection-item:first-child');
+
+val = listItem;
+val = list;
+
+// Get child nodes
+val = list.childNodes;
+val = list.childNodes[0];
+val = list.childNodes[0].nodeName;
+val = list.childNodes[3].nodeType;
+
+// 1 - Element
+// 2 - Attribute (deprecated)
+// 3 - Text node
+// 8 - Comment
+// 9 - Document itself
+// 10 - Doctype
+
+
+// Get children element nodes
+val = list.children;
+val = list.children[1];
+list.children[1].textContent = 'Hello';
+// Children of children
+list.children[3].children[0].id = 'test-link';
+val = list.children[3].children[0];
+
+// First child
+val = list.firstChild;
+val = list.firstElementChild;
+
+// Last child
+val = list.lastChild;
+val = list.lastElementChild;
+
+// Count child elements
+val = list.childElementCount;
+
+// Get parent node
+val = listItem.parentNode;
+val = listItem.parentElement;
+val = listItem.parentElement.parentElement;
+
+// Get next sibling
+val = listItem.nextSibling;
+val = listItem.nextElementSibling.nextElementSibling.previousElementSibling;
+
+// Get prev sibling
+val = listItem.previousSibling;
+val = listItem.previousElementSibling;
+console.log(val);
