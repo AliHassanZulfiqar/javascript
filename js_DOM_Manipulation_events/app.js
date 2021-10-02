1f1a15e21b74c7c2bcd0c0d4749f58f1c17eff54
Ali Hassan <bc200401060@vu.edu.pk>
2021-10-02 20:11:24 +0500

    DOM selectors /Section-2

--- js_DOM_Manipulation_events/app.js
+++ js_DOM_Manipulation_events/app.js
@@ -50,8 +50,9 @@ scriptsArr.forEach(function(script) {
 console.log(val);
 */
 
-// 2: Selectors
+// 2: Single Element Selectors
 
+/*
 // document.getElementById():
 
 // console.log(document.getElementById('task-title'));
@@ -84,3 +85,57 @@ document.querySelector('ul li').style.color = '#ff4040'; // still, it will only 
 document.querySelector('li:last-child').style.color = '#ff4040'; // it will only select the last one
 document.querySelector('li:nth-child(3)').style.color = '#a6c020'; // it will only select the custom one
 document.querySelector('li:nth-child(odd)').style.background = '#333'; // it will only select the last one
+*/
+
+// 2: Multiple Element Selectors
+
+// document.getElementsByClassName
+// const item = document.getElementsByClassName('collection-item'); //this selects all the class items in the whole document but we should not do it on a global scope
+// console.log(item);
+// console.log(item[0]); // we can also use array method here to select a specific one
+// item[0].style.color = 'blue';
+// item[0].textContent = 'blue';
+
+// const listItems = document.querySelector('ul').getElementsByClassName('collection-item'); // this way we selected only classes within ul
+
+// console.log(listItems);
+
+// document.getElementsByTagName
+// let tag = document.getElementsByTagName('li'); //this selects all specified html tags in the whole document but we should not do it on a global scope
+// console.log(tag);
+// console.log(tag[0]); // we can also use array method here to select a specific one
+// tag[0].style.color = 'blue';
+// tag[0].textContent = 'blue';
+
+// // Convert HTML collection into array
+// tag = Array.from(tag);
+
+// // tag.reverse();
+
+// tag.forEach(function(li, index){
+//   console.log(li.className);
+//   li.textContent = `${index}: Hello`;
+// });
+
+// console.log(tag);
+
+// document.querySelectorsAll():
+const items = document.querySelectorAll('ul.collection li.collection-item');
+
+items.forEach(function(item, index){
+    item.textContent = `${index}: Hello`;
+});
+
+const liOdd = document.querySelectorAll('li:nth-child(odd)');
+const liEven = document.querySelectorAll('li:nth-child(even)');
+
+liOdd.forEach(function(li, index){
+  li.style.background = "#333";
+  li.style.color = "#fff";
+});
+
+for(let i=0; i < liEven.length; i++){
+    liEven[i].style.background = '#CCC';
+};
+
+console.log(items);
