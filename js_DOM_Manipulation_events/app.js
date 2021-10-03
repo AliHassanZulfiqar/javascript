8423dab6d2dac7d18fa3d8b1ebcbd4ceb570d814
Ali Hassan <bc200401060@vu.edu.pk>
2021-10-03 18:06:07 +0500

    Keyboard Input Events /Section-2

--- js_DOM_Manipulation_events/app.js
+++ js_DOM_Manipulation_events/app.js
@@ -349,6 +349,7 @@ function onClick(e){
 
 // 7: Mouse Events 
 
+/*
 const clearBtn = document.querySelector('.clear-tasks');
 const card = document.querySelector('.card');
 const heading = document.querySelector('h5');
@@ -379,4 +380,49 @@ function runEvent(e) {
   heading.textContent= `MouseX: ${e.offsetX} MouseY: ${e.offsetY}`;
 
   document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 40)`;
-}
+};
+*/
+
+// 8: Keyboard Input Events 
+
+const form = document.querySelector('form');
+const taskInput = document.getElementById('task');
+const heading = document.querySelector('h5');
+const select = document.querySelector('select');
+
+// Clear input
+taskInput.value = '';
+
+// form.addEventListener('submit', runEvent);
+
+// Keydown
+//taskInput.addEventListener('keydown', runEvent);
+// Keydown
+// taskInput.addEventListener('keyup', runEvent);
+// Keypress
+// taskInput.addEventListener('keypress', runEvent);
+// Focus
+// taskInput.addEventListener('focus', runEvent);
+// Blur
+// taskInput.addEventListener('blur', runEvent);
+// Cut
+// taskInput.addEventListener('cut', runEvent);
+// Paste
+// taskInput.addEventListener('paste', runEvent);
+// Input
+// taskInput.addEventListener('input', runEvent);
+// Change
+select.addEventListener('change', runEvent); // we are gonna have to comment css to see select list here
+
+function runEvent(e){
+  console.log(`EVENT TYPE: ${e.type}`);
+
+  //console.log(e.target.value);
+
+  // heading.innerText = e.target.value;
+
+  // Get input value
+  // console.log(taskInput.value);
+
+  // e.preventDefault();
+};
